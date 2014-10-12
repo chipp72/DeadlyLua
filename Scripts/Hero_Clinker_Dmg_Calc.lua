50f1d8c94decbc7b6c4b89344eabd633b4b72a20
top 1 <SkyIce39@gmail.com>
2014-10-12 13:29:07 +0300

    Update Hero_Clinker_Dmg_Calc.lua

--- Scripts/Hero_Clinker_Dmg_Calc.lua
+++ Scripts/Hero_Clinker_Dmg_Calc.lua
@@ -2,50 +2,51 @@
 --Подключение библиотеки
 require("libs.Utils")
 
---создаем таблицу
+-- создаем таблицу
 hero = {}
 
---Шрифт
+-- Шрифт
 F16 = drawMgr:CreateFont("F16","Arial",16,500)
 
---Таблицы урона
+-- Таблицы урона скиллов
 dmgL = {80,160,240,320}
 dmgR = {100,175,250,325}
 dmg = {400,500,600,700,800}
 
 
---Главная функция
+-- Главная функция
 function Tick( tick )
 
--- Проверка играем ли мы
+-- Проверка играем ли мы, а также добавляем задержку в 200 мс.
 	if not client.connected or client.loading or client.console or not SleepCheck() then return end
 	Sleep(200)
--- Мой герой
-
+	
+-- Определняем своего героя
 	local me = entityList:GetMyHero() 
 	
+-- Если нету героя тогда возращаемся обратно	
 	if not me then return end
 	
--- Проверка на героя
+-- Проверка на конкретного героя (тинкера) героя
 	if me.classId ~= CDOTA_Unit_Hero_Tinker then
 		script:Disable()
 		return
 	end
 
--- Позиция экрана и назначения скилов
+-- Обозначаем способности
 	local laser = me:GetAbility(1)
 	local rocket = me:GetAbility(2)
 	
--- Проверка спеллов
+-- Если оба скилла 0 уровня тогда возращаемся обратно
 	if laser.level == 0 and rocket.level == 0 then
 		return
 	end
 
--- Определение дагона
+-- Определение дагона и эзериала
 	local dagon = me:FindDagon()
 	local eb = me:FindItem("item_ethereal_blade")
 	
--- Модификатор урона с дагона
+-- Рассчет урона с дагона в зависомти от его уровня
 	if dagon then
 		local lvl = string.match (dagon.name, "%d+")
 		if not lvl then lvl = 1 end dmgD = dmg[lvl*1]
@@ -54,9 +55,9 @@ function Tick( tick )
 -- Таблица героев
 	local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO, team = (5-me.team), illusion=false})
 	
-	--Калькулятор урона
+	--Для каждого юнита в таблице
 		for i,v in ipairs(enemies) do
-					
+		
 		local offset = v.healthbarOffset
 		
 		if offset == -1 then return end
