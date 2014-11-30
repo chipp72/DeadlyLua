5d191261b4a51a85c0fd712d75573a9ed832b09b
has left <SkyIce39@gmail.com>
2014-12-01 02:51:45 +0300

    Update Single_Range_Display.lua

--- Scripts/Single_Range_Display.lua
+++ Scripts/Single_Range_Display.lua
@@ -1,7 +1,7 @@
 --<<Shows blink dager range>>
 require("libs.ScriptConfig")
 
-config = ScriptConfig.new()
+local config = ScriptConfig.new()
 config:SetParameter("Hotkey", "O", config.TYPE_HOTKEY)
 config:SetParameter("Range", 1200)
 config:Load()
@@ -11,10 +11,11 @@ local key = config.Hotkey
 
 local activated = false
 local effect = nil
+local play = false
 
 function Key(msg,code)
 	-- check if ingame and not chatting
-    if msg ~= KEY_UP or code ~= key or client.chat or not client.connected or client.loading or client.console then
+    if msg ~= KEY_UP or code ~= key or client.chat or client.console then
     	return
     end
 
@@ -35,12 +36,29 @@ function Key(msg,code)
 		RemoveEffect()
 	end    
 end
- 
+
+function Load()
+	if PlayingGame() then
+		play = true
+		script:RegisterEvent(EVENT_KEY,Key)
+		script:UnregisterEvent(Load)
+	end
+end
+
 function RemoveEffect()
 	effect = nil
 	collectgarbage("collect")
 	activated = false
 end
- 
-script:RegisterEvent(EVENT_CLOSE,RemoveEffect) -- remove effect on game close
-script:RegisterEvent(EVENT_KEY,Key)
+
+function GameClose()
+	RemoveEffect()
+	if play then
+		script:UnregisterEvent(Key)
+		script:RegisterEvent(EVENT_TICK,Load)
+		play = false
+	end
+end
+
+script:RegisterEvent(EVENT_TICK,Load)
+script:RegisterEvent(EVENT_CLOSE,GameClose)
