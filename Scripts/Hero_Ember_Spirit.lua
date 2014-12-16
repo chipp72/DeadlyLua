2b555071da630654dc14ec19a812a30bff6b0457
has left <SkyIce39@gmail.com>
2014-12-16 14:40:32 +0300

    Update Hero_Ember_Spirit.lua

--- Scripts/Hero_Ember_Spirit.lua
+++ Scripts/Hero_Ember_Spirit.lua
@@ -8,26 +8,49 @@ config:SetParameter("Hotkey", "W", config.TYPE_HOTKEY)
 config:Load()
 
 local key = config.Hotkey
+local play = false
 
-function Key(msg,code)
 
-	if code ~= key or client.chat or not client.connected or client.loading or client.console then	return end
+function EmberKey(msg,code)
+
+	if code ~= key or client.chat then return end
 
 	local me = entityList:GetMyHero() if not me then return end
 
-	if me.classId ~= CDOTA_Unit_Hero_EmberSpirit then
-		script:Disable()
-	else
-		local enemy = entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive = true,team = (5-me.team),illusion = false})
-		local w_ = me:GetAbility(1)
-		if w_.state == -1 and me:DoesHaveModifier("modifier_ember_spirit_sleight_of_fist_caster") then
-			for i,v in ipairs(enemy) do
-				if v.health > 0 and not v:IsMagicDmgImmune() and me:GetDistance2D(v) < 400 then
-					me:CastAbility(w_)
-				end
+
+	local enemy = entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive = true,team = (5-me.team),illusion = false})
+	local w_ = me:GetAbility(1)
+	if w_.state == -1 and me:DoesHaveModifier("modifier_ember_spirit_sleight_of_fist_caster") then
+		for i,v in ipairs(enemy) do
+			if v.health > 0 and not v:IsMagicDmgImmune() and me:GetDistance2D(v) < 400 then
+				me:CastAbility(w_)
 			end
-		end			
+		end
+	end			
+
+end
+
+
+function Load()
+	if PlayingGame() then
+		local me = entityList:GetMyHero()	
+		if me.classId == CDOTA_Unit_Hero_EmberSpirit then			
+			play = true
+			script:RegisterEvent(EVENT_KEY,EmberKey)
+			script:UnregisterEvent(Load)
+		else
+			script:Disable()
+		end
 	end
 end
 
-script:RegisterEvent(EVENT_KEY,Key)
+function GameClose()	
+	if play then
+		script:UnregisterEvent(EmberKey)
+		script:RegisterEvent(EVENT_TICK,Load)
+		stuff.play = false
+	end
+end
+
+script:RegisterEvent(EVENT_TICK,Load)
+script:RegisterEvent(EVENT_CLOSE,GameClose)
