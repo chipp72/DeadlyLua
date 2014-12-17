dc7e8ea2a3ca21117744edbae8fb6fc8c98cbea7
has left <SkyIce39@gmail.com>
2014-12-17 03:20:08 +0300

    Update Hero_Ember_Spirit.lua

--- Scripts/Hero_Ember_Spirit.lua
+++ Scripts/Hero_Ember_Spirit.lua
@@ -9,13 +9,23 @@ config:Load()
 
 local key = config.Hotkey
 local play = false
+local activated = false
 
 function EmberKey(msg,code)
 
-	if code ~= key or client.chat then return end
+	if msg == KEY_DOWN and not client.chat and code == key then
+		activated = true
+	else
+		activated = false
+	end
 
-	local me = entityList:GetMyHero() if not me then return end
+end
 
+function EmberTick(tick)
+
+	if not activated or not SleepCheck() then return end
+
+	local me = entityList:GetMyHero()
 
 	local enemy = entityList:GetEntities({type=LuaEntity.TYPE_HERO,alive = true,team = (5-me.team),illusion = false})
 	local w_ = me:GetAbility(1)
@@ -23,11 +33,14 @@ function EmberKey(msg,code)
 		for i,v in ipairs(enemy) do
 			if v.health > 0 and not v:IsMagicDmgImmune() and me:GetDistance2D(v) < 400 then
 				me:CastAbility(w_)
+				activated = false
+				Sleep(1000)
 			end
 		end
 	end			
 
 end
+
 
 function Load()
 	if PlayingGame() then
@@ -35,6 +48,7 @@ function Load()
 		if me.classId == CDOTA_Unit_Hero_EmberSpirit then			
 			play = true
 			script:RegisterEvent(EVENT_KEY,EmberKey)
+			script:RegisterEvent(EVENT_FRAME,EmberTick)
 			script:UnregisterEvent(Load)
 		else
 			script:Disable()
@@ -45,8 +59,10 @@ end
 function GameClose()	
 	if play then
 		script:UnregisterEvent(EmberKey)
-		script:RegisterEvent(EVENT_TICK,Load)
+		script:UnregisterEvent(EmberTick)
+		script:RegisterEvent(EVENT_TICK,Load)		
 		play = false
+		activated = false
 	end
 end
 
